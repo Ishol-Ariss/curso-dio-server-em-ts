5f46e276ca5ff3043e573a49e764a1a64e912b32
David <david44mg@gmail.com>
2022-09-26 14:46:19 -0300

    automatizando o servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("oi")
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express()
+
+app.use('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'Mudou'})
+})
+
+app.listen(3333, () =>{
+    console.log("rodando!")
+})
