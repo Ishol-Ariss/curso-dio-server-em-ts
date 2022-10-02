349aec2a25fe27b860531d0b0e910928f451bf2b
David <david44mg@gmail.com>
2022-10-01 21:32:43 -0300

    modified

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express'
+import usersRoute from './routes/users.route'
 
 const app = express()
 
+app.use(usersRoute)
+
 app.use('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({foo: 'Mudou'})
+    res.status(200).send({foo: 'Mudou dnv'})
 })
 
 app.listen(3333, () =>{
